ed2eaf25e2c8d8aed1f5c3b2865100f9029fd941
Jesse <jessebeke10@gmail.com>
2024-06-18 14:19:45 +0100

    feat: Added tabs to schedule pahe

--- src/pages/SchedulePage.jsx
+++ src/pages/SchedulePage.jsx
@@ -3,10 +3,10 @@ import { useParams } from "react-router-dom";
 import { fetchSchedule } from "../services/FirestoreService";
 import SideNav from "../components/SideNav";
 import TopNav from "../components/TopNav";
+import { Tab, TabGroup, TabList, TabPanel, TabPanels } from "@headlessui/react";
 
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
-
 
 const SchedulePage = () => {
   const { documentId } = useParams();
@@ -45,20 +45,40 @@ const SchedulePage = () => {
           <div className="main flex flex-grow flex-col transition-all duration-150 ease-in md:ml-0">
             <div className="p-3">
               <div className="bg-white mt-5 rounded-[30px]">
-                schedulepage
-                <div className="p-10">
-                  <FullCalendar
-                    plugins={[dayGridPlugin]}
-                    initialView="dayGridMonth"
-                    weekends={false}
-                    events={events}
-                    headerToolbar={{
-                      left: "prev,next today",
-                      center: "title",
-                      right: "dayGridMonth,dayGridWeek,dayGridDay",
-                    }}
-                    eventContent={renderEventContent}
-                  />
+                <div className="p-3">
+                  <TabGroup>
+                    <TabList className="flex gap-4 bg-[#F8F9FC] w-fit px-4 py-3 rounded-full">
+                      <Tab className="rounded-full py-1 px-3 text-sm/6 font-semibold text-black focus:outline-none data-[selected]:bg-blue-500 data-[selected]:text-white">
+                        Timeline
+                      </Tab>
+                      <Tab className="rounded-full py-1 px-3 text-sm/6 font-semibold text-black focus:outline-none data-[selected]:bg-blue-500 data-[selected]:text-white">
+                        Kanban
+                      </Tab>
+                      <Tab className="rounded-full py-1 px-3 text-sm/6 font-semibold text-black focus:outline-none data-[selected]:bg-blue-500 data-[selected]:text-white">
+                        List
+                      </Tab>
+                    </TabList>
+                    <TabPanels>
+                      <TabPanel>
+                        <div className="p-10">
+                          <FullCalendar
+                            plugins={[dayGridPlugin]}
+                            initialView="dayGridMonth"
+                            weekends={false}
+                            events={events}
+                            headerToolbar={{
+                              left: "prev,next today",
+                              center: "title",
+                              right: "dayGridMonth,dayGridWeek,dayGridDay",
+                            }}
+                            eventContent={renderEventContent}
+                          />
+                        </div>
+                      </TabPanel>
+                      <TabPanel>Content 2</TabPanel>
+                      <TabPanel>Content 3</TabPanel>
+                    </TabPanels>
+                  </TabGroup>
                 </div>
               </div>
             </div>
